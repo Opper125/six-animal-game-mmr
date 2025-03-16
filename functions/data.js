21f0435f22ea23f9661130763c4d0727a56d1e69
Opper125 <crakb68@gmail.com>
2025-03-16 23:20:51 +0630

    Delete functions directory

--- functions/data.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
-exports.handler = async (event, context) => {
-    const { data, error } = await supabase.from('bets').select('*').eq('round', Math.floor(Date.now() / 20000));
-    if (error) return { statusCode: 500, body: JSON.stringify(error) };
-    return { statusCode: 200, body: JSON.stringify(data) };
-};
